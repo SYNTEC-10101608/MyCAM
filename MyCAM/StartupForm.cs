0b30bc2a73b82e8374f54f548bea5fabd6cc411d
王棕賢 <jason.wang@syntecclub.com>
2024-12-18 19:34:22 +0800

    可以縫補，但識別 bound 還有錯

--- MyCAM/StartupForm.cs
+++ MyCAM/StartupForm.cs
@@ -7,9 +7,11 @@ using OCC.ShapeAnalysis;
 using OCC.TopAbs;
 using OCC.TopExp;
 using OCC.TopoDS;
+using OCC.TopTools;
 using OCCViewer;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace MyCAM
@@ -81,17 +83,19 @@ namespace MyCAM
 		void ImportModel( ModelFormat format )
 		{
 			ImportHandler.ImportModel( format, out TopoDS_Shape theShape );
+			if( theShape == null ) {
+				MessageBox.Show( "Failed to import the model" );
+				return;
+			}
+
+			// sew the shape
+			theShape = SewShape( new List<TopoDS_Shape>() { theShape } );
 			ShowModel( theShape );
 			m_tsmiExtractFace.Enabled = true;
 		}
 
 		void ShowModel( TopoDS_Shape theShape )
 		{
-			if( theShape == null ) {
-				MessageBox.Show( "Failed to import the model" );
-				return;
-			}
-
 			// create AIS_Shape
 			AIS_Shape aisShape = new AIS_Shape( theShape );
 			Graphic3d_MaterialAspect aspect = new Graphic3d_MaterialAspect( Graphic3d_NameOfMaterial.Graphic3d_NOM_STEEL );
@@ -112,6 +116,105 @@ namespace MyCAM
 			ActionMode = EActionMode.ExtractFace;
 		}
 
+		void ShowExtrctedFace()
+		{
+			// get selected face
+			List<TopoDS_Face> faceList = GetSelectedFace();
+			if( faceList.Count == 0 ) {
+				MessageBox.Show( "No face is selected" );
+				return;
+			}
+
+			// sew the faces
+			TopoDS_Shape sewResult = SewShape( faceList.Cast<TopoDS_Shape>().ToList() );
+			List<TopoDS_Wire> boundaryWireList = GetAllFreeBound( sewResult );
+			m_viewer.GetAISContext().RemoveAll( false );
+
+			TopTools_IndexedDataMapOfShapeListOfShape map = new TopTools_IndexedDataMapOfShapeListOfShape();
+			TopExp.MapShapesAndAncestors( sewResult, TopAbs_ShapeEnum.TopAbs_EDGE, TopAbs_ShapeEnum.TopAbs_FACE, ref map );
+
+			// display the faces
+			AIS_Shape faceAIS = new AIS_Shape( sewResult );
+			Graphic3d_MaterialAspect aspect = new Graphic3d_MaterialAspect( Graphic3d_NameOfMaterial.Graphic3d_NOM_STEEL );
+			faceAIS.SetMaterial( aspect );
+			faceAIS.SetDisplayMode( 1 );
+			m_viewer.GetAISContext().Display( faceAIS, false );
+
+			// display the boundary wires
+			foreach( TopoDS_Wire wire in boundaryWireList ) {
+				AIS_Shape oneWireAIS = new AIS_Shape( wire );
+				oneWireAIS.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_RED ) );
+				oneWireAIS.SetWidth( 3 );
+				m_viewer.GetAISContext().Display( oneWireAIS, false );
+			}
+			m_viewer.AxoView();
+			m_viewer.ZoomAllView();
+			m_viewer.UpdateView();
+		}
+
+		TopoDS_Shape SewShape( List<TopoDS_Shape> shapeList )
+		{
+			// split the faces into shells
+			double dSewingTolerance = 0.001;
+			BRepBuilderAPI_Sewing sewing = new BRepBuilderAPI_Sewing( dSewingTolerance );
+			foreach( TopoDS_Shape shape in shapeList ) {
+				sewing.Add( shape );
+			}
+			sewing.Perform();
+			return sewing.SewedShape();
+		}
+
+		List<TopoDS_Wire> GetAllFreeBound( TopoDS_Shape sewResult )
+		{
+			List<TopoDS_Shape> faceGroupList = new List<TopoDS_Shape>();
+
+			// all selected faces are in the same shell
+			if( sewResult.shapeType == TopAbs_ShapeEnum.TopAbs_SHELL ) {
+				faceGroupList.Add( TopoDS.ToShell( sewResult ) );
+			}
+			else {
+
+				// get all shells
+				TopExp_Explorer shellExp = new TopExp_Explorer( sewResult, TopAbs_ShapeEnum.TopAbs_SHELL );
+				while( shellExp.More() ) {
+					faceGroupList.Add( TopoDS.ToShell( shellExp.Current() ) );
+					shellExp.Next();
+				}
+
+				// get faces not belong to any shell
+				List<TopoDS_Shape> freeFaceList = new List<TopoDS_Shape>();
+				TopExp_Explorer faceExp = new TopExp_Explorer( sewResult, TopAbs_ShapeEnum.TopAbs_FACE );
+				while( faceExp.More() ) {
+					TopoDS_Face face = TopoDS.ToFace( faceExp.Current() );
+
+					// the face is not in any shell
+					bool bInShell = false;
+					foreach( TopoDS_Shape shell in faceGroupList ) {
+						if( shell.elementsAsList.Contains( face ) ) {
+							bInShell = true;
+							break;
+						}
+					}
+					if( !bInShell ) {
+						freeFaceList.Add( face );
+					}
+					faceExp.Next();
+				}
+				faceGroupList.AddRange( freeFaceList );
+			}
+
+			List<TopoDS_Wire> wireList = new List<TopoDS_Wire>();
+			foreach( TopoDS_Shape faceGroup in faceGroupList ) {
+				ShapeAnalysis_FreeBounds freeBounds = new ShapeAnalysis_FreeBounds( faceGroup );
+				TopExp_Explorer wireExp = new TopExp_Explorer( freeBounds.GetClosedWires(), TopAbs_ShapeEnum.TopAbs_WIRE );
+				while( wireExp.More() ) {
+					wireList.Add( TopoDS.ToWire( wireExp.Current() ) );
+					wireExp.Next();
+				}
+			}
+			return wireList;
+		}
+
 		List<TopoDS_Face> GetSelectedFace()
 		{
 			List<TopoDS_Face> lstFace = new List<TopoDS_Face>();
@@ -124,80 +227,6 @@ namespace MyCAM
 				m_viewer.GetAISContext().NextSelected();
 			}
 			return lstFace;
-		}
-
-		void ShowExtrctedFace()
-		{
-			// get selected face
-			List<TopoDS_Face> faceList = GetSelectedFace();
-			if( faceList.Count == 0 ) {
-				MessageBox.Show( "No face is selected" );
-				return;
-			}
-
-			// split the faces into shells
-			double SewTolerance = 0.001;
-			BRepBuilderAPI_Sewing sewer = new BRepBuilderAPI_Sewing( SewTolerance );
-			foreach( TopoDS_Face face in faceList ) {
-				sewer.Add( face );
-			}
-			sewer.Perform();
-			TopoDS_Shape theShape = sewer.SewedShape();
-			List<TopoDS_Shape> shellList = new List<TopoDS_Shape>();
-			if( theShape.shapeType == TopAbs_ShapeEnum.TopAbs_SHELL ) {
-
-				// all selected faces are in the same shell
-				shellList.Add( TopoDS.ToShell( theShape ) );
-			}
-			else {
-				TopExp_Explorer shellExp = new TopExp_Explorer( theShape, TopAbs_ShapeEnum.TopAbs_SHELL );
-				while( shellExp.More() ) {
-					shellList.Add( TopoDS.ToShell( shellExp.Current() ) );
-					shellExp.Next();
-				}
-
-				// get faces not belong to any shell
-				List<TopoDS_Shape> freeFaceList = new List<TopoDS_Shape>();
-				TopExp_Explorer faceExp = new TopExp_Explorer( theShape, TopAbs_ShapeEnum.TopAbs_FACE );
-				while( faceExp.More() ) {
-					TopoDS_Face face = TopoDS.ToFace( faceExp.Current() );
-					if( !shellList.Exists( shell => shell.elementsAsList.Contains( face ) ) ) {
-						freeFaceList.Add( face );
-					}
-					faceExp.Next();
-				}
-				shellList.AddRange( freeFaceList );
-			}
-
-			// get all bondary wire of the extracted faces
-			List<TopoDS_Wire> wireList = new List<TopoDS_Wire>();
-			foreach( TopoDS_Shape shell in shellList ) {
-				ShapeAnalysis_FreeBounds freeBounds = new ShapeAnalysis_FreeBounds( shell );
-				TopExp_Explorer wireExp = new TopExp_Explorer( freeBounds.GetClosedWires(), TopAbs_ShapeEnum.TopAbs_WIRE );
-				while( wireExp.More() ) {
-					wireList.Add( TopoDS.ToWire( wireExp.Current() ) );
-					wireExp.Next();
-				}
-			}
-
-			m_viewer.GetAISContext().RemoveAll( false );
-			foreach( TopoDS_Face face in faceList ) {
-				AIS_Shape aisShape = new AIS_Shape( face );
-				Graphic3d_MaterialAspect aspect = new Graphic3d_MaterialAspect( Graphic3d_NameOfMaterial.Graphic3d_NOM_STEEL );
-				aisShape.SetMaterial( aspect );
-				aisShape.SetDisplayMode( 1 );
-				m_viewer.GetAISContext().Display( aisShape, false );
-			}
-			foreach( TopoDS_Wire wire in wireList ) {
-				AIS_Shape aisShape = new AIS_Shape( wire );
-				Graphic3d_MaterialAspect aspect = new Graphic3d_MaterialAspect( Graphic3d_NameOfMaterial.Graphic3d_NOM_STEEL );
-				aisShape.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_RED ) );
-				aisShape.SetWidth( 3 );
-				m_viewer.GetAISContext().Display( aisShape, false );
-			}
-			m_viewer.AxoView();
-			m_viewer.ZoomAllView();
-			m_viewer.UpdateView();
 		}
 
 		// action
