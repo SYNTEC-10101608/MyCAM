fcb2d09eb279dce28cc3f05f649f6b124a10c921
王棕賢 <jason.wang@syntecclub.com>
2024-12-27 14:04:04 +0800

    新增放置零件模組空殼

---
+++ PartPlacement/PartPlacementForm.cs
@@ -0,0 +1,34 @@
+﻿using OCC.TopoDS;
+using OCCViewer;
+using System;
+using System.Windows.Forms;
+
+namespace PartPlacement
+{
+	public partial class PartPlacementForm : Form
+	{
+		public Action<TopoDS_Shape> ImportOK;
+
+		public PartPlacementForm( TopoDS_Shape modelShape )
+		{
+			InitializeComponent();
+
+			// create the viewer
+			bool bSucess = m_OCCViewer.InitViewer( m_panViewer );
+			if( !bSucess ) {
+				MessageBox.Show( ToString() + "Init Error: Init Viewer" );
+				return;
+			}
+			Controls.Add( m_panViewer );
+			m_panViewer.Dock = DockStyle.Fill;
+			m_OCCViewer.UpdateView();
+		}
+
+		// viewer
+		Panel m_panViewer = new Panel();
+		Viewer m_OCCViewer = new Viewer();
+
+		// import model
+		TopoDS_Shape m_ModelShape = null;
+	}
+}
