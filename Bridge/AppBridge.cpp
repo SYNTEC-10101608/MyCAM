352e1d543166a4859a30fb2e57e6456951ee9203
王棕賢 <jason.wang@syntecclub.com>
2025-04-23 13:19:09 +0800

    補上遺漏的實作

--- Bridge/AppBridge.cpp
+++ Bridge/AppBridge.cpp
@@ -23,7 +23,7 @@ public:
 	bool InitViewer( System::IntPtr theWnd )
 	{
 		// convert the IntPtr to a HWND
-		Handle( WNT_Window ) aWNTWindow = new WNT_Window( reinterpret_cast< HWND > ( theWnd.ToPointer() ) );
+		Handle( WNT_Window ) aWNTWindow = new WNT_Window( reinterpret_cast<HWND> (theWnd.ToPointer()) );
 		return myApp->InitViewer( aWNTWindow );
 	}
 
@@ -52,7 +52,7 @@ public:
 
 	void ZoomAtPoint( int theX1, int theY1, int theX2, int theY2 )
 	{
-
+		myApp->ZoomAtPoint( theX1, theY1, theX2, theY2 );
 	}
 
 	void StartZoomAtPoint( int theX, int theY )
