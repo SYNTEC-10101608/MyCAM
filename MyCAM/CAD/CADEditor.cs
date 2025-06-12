f49be7feba8e51686ad3c663ff5377de1c1e02c2
王棕賢 <jason.wang@syntecclub.com>
2025-06-12 10:28:05 +0800

    評估現階段沒必要特別分一個 cs 檔出來，先塞回 CADEditor 裡面

--- MyCAM/CAD/CADEditor.cs
+++ MyCAM/CAD/CADEditor.cs
@@ -1,7 +1,8 @@
 ﻿using OCC.AIS;
+using OCC.Graphic3d;
 using OCC.IFSelect;
 using OCC.IGESControl;
-using OCC.NCollection;
+using OCC.Quantity;
 using OCC.STEPControl;
 using OCC.TopAbs;
 using OCC.TopoDS;
@@ -33,6 +34,33 @@ namespace MyCAM.CAD
 		AxialParallel,
 		Plane,
 		PlaneParallel,
+	}
+
+	internal static class ViewHelper
+	{
+		public static AIS_Shape CreatePartAIS( TopoDS_Shape shape )
+		{
+			AIS_Shape aisShape = new AIS_Shape( shape );
+			Graphic3d_MaterialAspect aspect = new Graphic3d_MaterialAspect( Graphic3d_NameOfMaterial.Graphic3d_NOM_STEEL );
+			aisShape.SetMaterial( aspect );
+			aisShape.SetDisplayMode( (int)AIS_DisplayMode.AIS_Shaded );
+			aisShape.Attributes().SetFaceBoundaryDraw( true );
+			aisShape.Attributes().FaceBoundaryAspect().SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_BLACK ) );
+			Quantity_Color color = new Quantity_Color();
+			aisShape.Color( ref color );
+			Quantity_NameOfColor colorName = color.Name();
+			return aisShape;
+		}
+
+		public static AIS_Shape CreateFaceAIS( TopoDS_Shape shape )
+		{
+			AIS_Shape aisShape = new AIS_Shape( shape );
+			aisShape.SetDisplayMode( (int)AIS_DisplayMode.AIS_Shaded );
+			aisShape.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_GRAY50 ) );
+			aisShape.Attributes().SetFaceBoundaryDraw( true );
+			aisShape.Attributes().FaceBoundaryAspect().SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_BLACK ) );
+			return aisShape;
+		}
 	}
 
 	internal class CADEditor
