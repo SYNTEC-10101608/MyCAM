555bd19e2fd303925314670720a4caa2da8ae62d
王棕賢 <jason.wang@syntecclub.com>
2025-06-12 10:13:59 +0800

    畫一個 G54 座標在畫布上

--- MyCAM/StartupForm.cs
+++ MyCAM/StartupForm.cs
@@ -1,4 +1,8 @@
 ﻿using MyCAM.CAD;
+using OCC.AIS;
+using OCC.Geom;
+using OCC.gp;
+using OCC.Quantity;
 using OCCViewer;
 using System;
 using System.Windows.Forms;
@@ -22,6 +26,7 @@ namespace MyCAM
 
 			// CAD Editor
 			m_CADEditor = new CADEditor( m_Viewer, m_TreeView );
+			ShowG54Trihedron();
 		}
 
 		// app properties
@@ -29,6 +34,18 @@ namespace MyCAM
 
 		// CAD properties
 		CADEditor m_CADEditor;
+
+		void ShowG54Trihedron()
+		{
+			gp_Ax2 ax2 = new gp_Ax2();
+			AIS_Trihedron trihedron = new AIS_Trihedron( new Geom_Axis2Placement( ax2 ) );
+			trihedron.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_GRAY ) );
+			trihedron.SetSize( 100.0 );
+			trihedron.SetAxisColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_GRAY ) );
+			trihedron.SetTextColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_GRAY ) );
+			m_Viewer.GetAISContext().Display( trihedron, false );
+			m_Viewer.GetAISContext().Deactivate( trihedron );
+		}
 
 		// import part
 		void m_tsmiImportBRep_Click( object sender, EventArgs e )
