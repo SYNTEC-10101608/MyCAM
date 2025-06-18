c16fa22d07104b4435a5de8a40cf01d7d9960c79
王棕賢 <jason.wang@syntecclub.com>
2025-06-18 16:58:58 +0800

    1. 修正奇異點的檢查條件，考慮精度誤差 2. refine 選解不連續問題的 code 3. 將奇異點串聯 code 加回來

--- OCCTool/PostTool.cs
+++ OCCTool/PostTool.cs
@@ -22,44 +22,18 @@ namespace OCCTool
 					dM, dS, out double dM1, out double dS1, out double dM2, out double dS2 );
 
 				// sigular case, master has infinite solution
-				if( solveResult == 2 ) {
+				if( solveResult == 2
+					|| Math.Abs( dM1 ) < 1e-3 && Math.Abs( dS1 ) < 1e-3
+					|| Math.Abs( dM2 ) < 1e-3 && Math.Abs( dS2 ) < 1e-3 ) {
 					singularPointList.Add( true );
-				}
-				else {
-					singularPointList.Add( false );
-				}
-
-				if( Math.Abs( dM1 ) < 1e-3 && Math.Abs( dS1 ) < 1e-3 ) {
 					result.Add( new Tuple<double, double>( dM, 0 ) );
 					continue;
 				}
-
-				if( Math.Abs( dS1 * 180 / Math.PI - ( -0.600 ) ) < 1e-3 || Math.Abs( dS2 * 180 / Math.PI - ( -0.600 ) ) < 1e-3 ) {
-				}
+				singularPointList.Add( false );
 
 				// choose the closest solution
-				double dM1N = dM1 - 2 * Math.PI;
-				double dM1P = dM1 + 2 * Math.PI;
-				double diff1O = Math.Abs( dM - dM1 );
-				double diff1N = Math.Abs( dM - dM1N );
-				double diff1P = Math.Abs( dM - dM1P );
-				if( diff1N < diff1O && diff1N < diff1P ) {
-					dM1 = dM1N;
-				}
-				if( diff1P < diff1O && diff1P < diff1N ) {
-					dM1 = dM1P;
-				}
-				double dM2N = dM2 - 2 * Math.PI;
-				double dM2P = dM2 + 2 * Math.PI;
-				double diff2O = Math.Abs( dM - dM2 );
-				double diff2N = Math.Abs( dM - dM2N );
-				double diff2P = Math.Abs( dM - dM2P );
-				if( diff2N < diff2O && diff2N < diff2P ) {
-					dM2 = dM2N;
-				}
-				if( diff2P < diff2O && diff2P < diff2N ) {
-					dM2 = dM2P;
-				}
+				dM1 = FindClosetCoterminalAngle( dM, dM1 );
+				dM2 = FindClosetCoterminalAngle( dM, dM2 );
 				double diff1 = Math.Abs( dM - dM1 ) + Math.Abs( dS - dS1 );
 				double diff2 = Math.Abs( dM - dM2 ) + Math.Abs( dS - dS2 );
 				if( diff1 < diff2 ) {
@@ -74,21 +48,39 @@ namespace OCCTool
 			}
 
 			// refine the singular case
-			//int index = 0;
-			//while( index < result.Count ) {
-			//	int index0 = index;
-			//	while( index < result.Count - 1 && singularPointList[ index ] ) {
-			//		index++;
-			//	}
+			int index = 0;
+			while( index < result.Count ) {
+				int index0 = index;
+				while( index < result.Count - 1 && singularPointList[ index ] ) {
+					index++;
+				}
 
-			//	// interpolate C from index0 to index
-			//	double diffM = result[ index ].Item1 - result[ index0 ].Item1;
-			//	for( int i = index0; i < index; i++ ) {
-			//		result[ i ] = new Tuple<double, double>( result[ i ].Item1 + diffM / ( index - index0 ) * ( i - index0 ), 0 );
-			//	}
-			//	index++;
-			//}
+				// interpolate C from index0 to index
+				double diffM = result[ index ].Item1 - result[ index0 ].Item1;
+				for( int i = index0; i < index; i++ ) {
+					result[ i ] = new Tuple<double, double>( result[ i ].Item1 + diffM / ( index - index0 ) * ( i - index0 ), 0 );
+				}
+				index++;
+			}
 			return result;
+		}
+
+		static double FindClosetCoterminalAngle( double reference, double value )
+		{
+			double valueN = value - 2 * Math.PI;
+			double valueP = value + 2 * Math.PI;
+
+			double diffO = Math.Abs( reference - value );
+			double diffN = Math.Abs( reference - valueN );
+			double diffP = Math.Abs( reference - valueP );
+
+			if( diffN < diffO && diffN < diffP ) {
+				return valueN;
+			}
+			if( diffP < diffO && diffP < diffN ) {
+				return valueP;
+			}
+			return value;
 		}
 
 		static double[] ToolDirectionAtZero = new double[ 3 ] { 0, 0, 1 };
