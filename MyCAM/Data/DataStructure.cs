f986ec66e98999bd797c93db604ba0018770eb8b
王棕賢 <jason.wang@syntecclub.com>
2025-08-07 11:37:58 +0800

    導入顯示 CAM 路徑的功能，現在可以顯示路徑資訊了

--- MyCAM/Data/DataStructure.cs
+++ MyCAM/Data/DataStructure.cs
@@ -38,6 +38,31 @@ namespace MyCAM.Data
 		}
 	}
 
+	// path data
+	internal class PathData : ShapeData
+	{
+		public PathData( string szUID, TopoDS_Shape shapeData, List<PathEdge5D> pathElementList )
+			: base( szUID, shapeData )
+		{
+			m_CAMData = new CAMData( pathElementList );
+		}
+
+		public CAMData CAMData
+		{
+			get
+			{
+				return m_CAMData;
+			}
+		}
+
+		public override void DoTransform( gp_Trsf transform )
+		{
+			base.DoTransform( transform );
+		}
+
+		CAMData m_CAMData;
+	}
+
 	internal class PathEdge5D
 	{
 		public PathEdge5D( TopoDS_Edge pathEdge, TopoDS_Face componentFace )
@@ -57,56 +82,55 @@ namespace MyCAM.Data
 		}
 	}
 
-	// path data
-	internal class PathData : ShapeData
-	{
-		public PathData( string szUID, TopoDS_Shape shapeData, List<PathEdge5D> pathElementList )
-			: base( szUID, shapeData )
-		{
-		}
-
-		public override void DoTransform( gp_Trsf transform )
-		{
-			base.DoTransform( transform );
-		}
-	}
-
 	public class CADPoint
 	{
 		public CADPoint( gp_Pnt point, gp_Dir normalVec_1st, gp_Dir normalVec_2nd, gp_Dir tangentVec )
 		{
-			Point = point;
-			NormalVec_1st = normalVec_1st;
-			NormalVec_2nd = normalVec_2nd;
-			TangentVec = tangentVec;
+			m_Point = new gp_Pnt( point.X(), point.Y(), point.Z() );
+			m_NormalVec_1st = new gp_Dir( normalVec_1st.XYZ() );
+			m_NormalVec_2nd = new gp_Dir( normalVec_2nd.XYZ() );
+			m_TangentVec = new gp_Dir( tangentVec.XYZ() );
 		}
 
 		public gp_Pnt Point
 		{
-			get; private set;
+			get
+			{
+				return new gp_Pnt( m_Point.X(), m_Point.Y(), m_Point.Z() );
+			}
 		}
 
 		public gp_Dir NormalVec_1st
 		{
-			get; private set;
+			get
+			{
+				return new gp_Dir( m_NormalVec_1st.XYZ() );
+			}
 		}
 
 		// normal vector on co-face
 		public gp_Dir NormalVec_2nd
 		{
-			get; private set;
+			get
+			{
+				return new gp_Dir( m_NormalVec_2nd.XYZ() );
+			}
 		}
 
 		// tangent vector on path
 		public gp_Dir TangentVec
 		{
-			get; private set;
+			get
+			{
+				return new gp_Dir( m_TangentVec.XYZ() );
+			}
 		}
 
-		public CADPoint Clone()
-		{
-			return new CADPoint( Point, NormalVec_1st, NormalVec_2nd, TangentVec );
-		}
+		// using backing fields to prevent modified outside
+		gp_Pnt m_Point;
+		gp_Dir m_NormalVec_1st;
+		gp_Dir m_NormalVec_2nd;
+		gp_Dir m_TangentVec;
 	}
 
 	// currently assuming CAM = CAD + ToolVec
@@ -115,7 +139,7 @@ namespace MyCAM.Data
 		public CAMPoint( CADPoint cadPoint, gp_Dir toolVec )
 		{
 			CADPoint = cadPoint;
-			ToolVec = toolVec;
+			m_ToolVec = new gp_Dir( toolVec.XYZ() );
 		}
 
 		public CADPoint CADPoint
@@ -125,13 +149,18 @@ namespace MyCAM.Data
 
 		public gp_Dir ToolVec
 		{
-			get; private set;
+			get
+			{
+				return new gp_Dir( m_ToolVec.XYZ() );
+			}
+			private set
+			{
+				m_ToolVec = new gp_Dir( value.XYZ() );
+			}
 		}
 
-		public CAMPoint Clone()
-		{
-			return new CAMPoint( CADPoint.Clone(), new gp_Dir( ToolVec.XYZ() ) );
-		}
+		// using backing field to prevent modified outside
+		gp_Dir m_ToolVec;
 	}
 
 	internal class CAMData
--- MyCAM/Editor/CAMEditor.cs
+++ MyCAM/Editor/CAMEditor.cs
@@ -1,10 +1,17 @@
 ﻿using MyCAM.Data;
 using OCC.AIS;
+using OCC.BRepPrimAPI;
+using OCC.Geom;
+using OCC.gp;
+using OCC.Graphic3d;
+using OCC.Quantity;
 using OCC.ShapeAnalysis;
+using OCC.TCollection;
 using OCC.TopAbs;
 using OCC.TopExp;
 using OCC.TopoDS;
 using OCC.TopTools;
+using OCCTool;
 using OCCViewer;
 using System;
 using System.Collections.Generic;
@@ -45,6 +52,17 @@ namespace MyCAM.Editor
 		IEditorAction m_DefaultAction;
 		IEditorAction m_CurrentAction;
 
+		// for viewer resource handle
+		List<AIS_Line> m_ToolVecAISList = new List<AIS_Line>(); // need refresh, no need activate
+		List<AIS_Shape> m_OrientationAISList = new List<AIS_Shape>(); // need refresh, no need activate
+		List<AIS_TextLabel> m_IndexList = new List<AIS_TextLabel>(); // need refresh, no need activate
+		enum EvecType
+		{
+			ToolVec,
+			TangentVec,
+			NormalVec,
+		}
+		int m_nGap = 100;
 
 		// editor
 		public void EditStart()
@@ -153,7 +171,167 @@ namespace MyCAM.Editor
 
 			// update tree view and viewer
 			m_ViewManager.PathNode.ExpandAll();
+			ShowCAMData();
 			m_Viewer.UpdateView();
+		}
+
+		// view
+		void ShowCAMData()
+		{
+			ShowToolVec();
+			ShowOrientation();
+			ShowIndex();
+			m_Viewer.UpdateView();
+		}
+
+		void ShowToolVec()
+		{
+			// clear the previous tool vec
+			foreach( AIS_Line toolVecAIS in m_ToolVecAISList ) {
+				m_Viewer.GetAISContext().Remove( toolVecAIS, false );
+			}
+			m_ToolVecAISList.Clear();
+
+			// build tool vec
+			foreach( CAMData camData in GetCAMDataList() ) {
+				List<CAMPoint> filteredPath = camData.CAMPointList;
+				for( int i = 0; i < filteredPath.Count; i++ ) {
+					if( IsKeyToolVecIndex( i, camData, out bool bHL ) ) {
+						CAMPoint camPoint = filteredPath[ i ];
+						AIS_Line toolVecAIS = GetVecAIS( camPoint.CADPoint.Point, camPoint.ToolVec, EvecType.ToolVec );
+						if( bHL ) {
+							toolVecAIS.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_RED ) );
+							toolVecAIS.SetWidth( 4 );
+						}
+						m_ToolVecAISList.Add( toolVecAIS );
+					}
+				}
+			}
+
+			// display the tool vec
+			foreach( AIS_Line toolVecAIS in m_ToolVecAISList ) {
+				m_Viewer.GetAISContext().Display( toolVecAIS, false );
+				m_Viewer.GetAISContext().Deactivate( toolVecAIS );
+			}
+		}
+
+		void ShowOrientation()
+		{
+			// clear the previous orientation
+			foreach( AIS_Shape orientationAIS in m_OrientationAISList ) {
+				m_Viewer.GetAISContext().Remove( orientationAIS, false );
+			}
+			m_OrientationAISList.Clear();
+
+			// build orientation
+			foreach( CAMData camData in GetCAMDataList() ) {
+				gp_Pnt showPoint = camData.CAMPointList[ 0 ].CADPoint.Point;
+				gp_Dir orientationDir = new gp_Dir( camData.CAMPointList[ 0 ].CADPoint.TangentVec.XYZ() );
+				if( camData.IsReverse ) {
+					orientationDir.Reverse();
+				}
+				AIS_Shape orientationAIS = GetOrientationAIS( showPoint, orientationDir );
+				m_OrientationAISList.Add( orientationAIS );
+			}
+
+			// display the orientation
+			foreach( AIS_Shape orientationAIS in m_OrientationAISList ) {
+				m_Viewer.GetAISContext().Display( orientationAIS, false );
+				m_Viewer.GetAISContext().Deactivate( orientationAIS );
+			}
+		}
+
+		void ShowIndex()
+		{
+			// clear the previous text label
+			foreach( AIS_TextLabel textLabel in m_IndexList ) {
+				m_Viewer.GetAISContext().Remove( textLabel, false );
+			}
+			m_IndexList.Clear();
+
+			// create text label
+			int nCurrentIndex = 0;
+			foreach( CAMData camData in GetCAMDataList() ) {
+				gp_Pnt location = camData.CAMPointList[ 0 ].CADPoint.Point;
+				string szIndex = nCurrentIndex++.ToString();
+
+				// create text label ais
+				AIS_TextLabel textLabel = new AIS_TextLabel();
+				textLabel.SetText( new TCollection_ExtendedString( szIndex ) );
+				textLabel.SetPosition( location );
+				textLabel.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_WHITE ) );
+				textLabel.SetHeight( 20 );
+				textLabel.SetZLayer( (int)Graphic3d_ZLayerId.Graphic3d_ZLayerId_Topmost );
+				m_IndexList.Add( textLabel );
+			}
+
+			// display text label
+			foreach( AIS_TextLabel textLabel in m_IndexList ) {
+				m_Viewer.GetAISContext().Display( textLabel, false );
+				m_Viewer.GetAISContext().Deactivate( textLabel );
+			}
+		}
+
+		bool IsKeyToolVecIndex( int index, CAMData camData, out bool isToolVecMod )
+		{
+			// map CAD and CAM point index
+			int modifiedIndex = camData.IsReverse
+				? ( camData.CAMPointList.Count - 1 - index + camData.StartPoint ) % camData.CAMPointList.Count
+				: ( index + camData.StartPoint ) % camData.CAMPointList.Count;
+
+			// need highlight if the index is modified index
+			isToolVecMod = camData.GetToolVecModifyIndex().Contains( modifiedIndex );
+
+			// need to show if the index is key index
+			bool isEdgeStart = camData.EdgeStartIndex.Contains( modifiedIndex );
+			return index % m_nGap == 0 || isEdgeStart || isToolVecMod;
+		}
+
+		AIS_Line GetVecAIS( gp_Pnt point, gp_Dir dir, EvecType vecType )
+		{
+			gp_Pnt endPoint = new gp_Pnt( point.XYZ() + dir.XYZ() * 10 );
+			AIS_Line lineAIS = new AIS_Line( new Geom_CartesianPoint( point ), new Geom_CartesianPoint( endPoint ) );
+			switch( vecType ) {
+				case EvecType.ToolVec:
+					lineAIS.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_BLUE ) );
+					break;
+				case EvecType.TangentVec:
+					lineAIS.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_RED ) );
+					break;
+				case EvecType.NormalVec:
+					lineAIS.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_GREEN ) );
+					break;
+			}
+			lineAIS.SetWidth( 1 );
+			return lineAIS;
+		}
+
+		AIS_Shape GetOrientationAIS( gp_Pnt point, gp_Dir dir )
+		{
+			// draw a cone to indicate the orientation
+			gp_Ax2 coneAx2 = new gp_Ax2( point, dir );
+			BRepPrimAPI_MakeCone coneMaker = new BRepPrimAPI_MakeCone( coneAx2, 0.5, 0, 2 );
+			AIS_Shape coneAIS = new AIS_Shape( coneMaker.Shape() );
+			Graphic3d_MaterialAspect aspect = new Graphic3d_MaterialAspect( Graphic3d_NameOfMaterial.Graphic3d_NOM_STEEL );
+			coneAIS.SetMaterial( aspect );
+			coneAIS.SetColor( new Quantity_Color( Quantity_NameOfColor.Quantity_NOC_WHITE ) );
+			coneAIS.SetDisplayMode( (int)AIS_DisplayMode.AIS_Shaded );
+			coneAIS.SetZLayer( (int)Graphic3d_ZLayerId.Graphic3d_ZLayerId_Topmost );
+			return coneAIS;
+		}
+
+		List<CAMData> GetCAMDataList()
+		{
+			List<CAMData> camDataList = new List<CAMData>();
+			foreach( string pathID in m_CADManager.PathIDList ) {
+				if( m_CADManager.ShapeDataMap.TryGetValue( pathID, out ShapeData shapeData ) ) {
+					CAMData camData = ( (PathData)shapeData ).CAMData;
+					if( camData != null ) {
+						camDataList.Add( camData );
+					}
+				}
+			}
+			return camDataList;
 		}
 
 		// edit actions
