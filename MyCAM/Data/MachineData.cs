076049e80cdd24d3fca497011a6a9c8756c58f09
王棕賢 <jason.wang@syntecclub.com>
2025-08-13 19:57:23 +0800

    新增機構描述相關的類別

---
+++ MyCAM/Data/MachineData.cs
@@ -0,0 +1,131 @@
+﻿using MyCAM.Machine;
+using OCC.gp;
+using System.Collections.Generic;
+
+namespace MyCAM.Data
+{
+	internal class MachineTreeNode
+	{
+		public MachineTreeNode( MachineComponentType type )
+		{
+			Type = type;
+			Children = new List<MachineTreeNode>();
+		}
+
+		public MachineComponentType Type
+		{
+			get; private set;
+		}
+
+		public List<MachineTreeNode> Children
+		{
+			get; private set;
+		}
+
+		public void AddChild( MachineTreeNode child )
+		{
+			if( child != null ) {
+				Children.Add( child );
+			}
+		}
+	}
+
+	internal class MachineData
+	{
+		public MachineData()
+		{
+			RootNode = new MachineTreeNode( MachineComponentType.Base );
+		}
+
+		public virtual FiveAxisType FiveAxisType
+		{
+			get
+			{
+				return FiveAxisType.None;
+			}
+		}
+
+		public ToolDirection ToolDirection
+		{
+			get; set;
+		}
+
+		public RotaryAxis RotaryAxis
+		{
+			get; set;
+		}
+
+		public RotaryDirection RotaryDirection
+		{
+			get; set;
+		}
+
+		public MachineTreeNode RootNode
+		{
+			get; private set;
+		}
+	}
+
+	internal class SpindleTypeMachineData : MachineData
+	{
+		public override FiveAxisType FiveAxisType
+		{
+			get
+			{
+				return FiveAxisType.Spindle;
+			}
+		}
+
+		public gp_Vec ToolToSlaveVec
+		{
+			get; set;
+		}
+
+		public gp_Vec SlaveToMasterVec
+		{
+			get; set;
+		}
+	}
+
+	internal class TableTypeMachineData : MachineData
+	{
+		public override FiveAxisType FiveAxisType
+		{
+			get
+			{
+				return FiveAxisType.Table;
+			}
+		}
+
+		public gp_Vec MasterToSlaveVec
+		{
+			get; set;
+		}
+
+		public gp_Vec MCSToMasterVec
+		{
+			get; set;
+		}
+	}
+
+	internal class MixTypeMachineData : MachineData
+	{
+		public override FiveAxisType FiveAxisType
+		{
+			get
+			{
+				return FiveAxisType.Mix;
+			}
+		}
+
+		public gp_Vec ToolToMasterVec
+		{
+			get; set;
+		}
+
+		public gp_Vec MCSToSlaveVec
+		{
+			get; set;
+		}
+	}
+}
---
+++ MyCAM/Machine/Define.cs
@@ -0,0 +1,45 @@
+﻿namespace MyCAM.Machine
+{
+	public enum FiveAxisType
+	{
+		None = 0,
+		Spindle = 1,
+		Table = 2,
+		Mix = 3,
+	}
+
+	public enum ToolDirection
+	{
+		None = 0,
+		X = 1,
+		Y = 2,
+		Z = 3,
+	}
+
+	public enum RotaryAxis
+	{
+		None = 0,
+		X = 1,
+		Y = 2,
+		Z = 3,
+	}
+
+	public enum RotaryDirection
+	{
+		None = 0,
+		RightHand = 1,
+		LeftHand = 2,
+	}
+
+	public enum MachineComponentType
+	{
+		Base = 0,
+		XAxis = 1,
+		YAxis = 2,
+		ZAxis = 3,
+		Master = 4,
+		Slave = 5,
+		WorkPiece = 6,
+		Tool = 7,
+	}
+}
