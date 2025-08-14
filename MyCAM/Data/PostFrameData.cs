7db4ba995d4088884535ca1bf00520b416a58b46
王棕賢 <jason.wang@syntecclub.com>
2025-08-14 08:51:27 +0800

    新增 PostFrameData

---
+++ MyCAM/Data/PostFrameData.cs
@@ -0,0 +1,30 @@
+﻿namespace MyCAM.Data
+{
+	internal class PostFrameData
+	{
+		public double X
+		{
+			get; set;
+		}
+
+		public double Y
+		{
+			get; set;
+		}
+
+		public double Z
+		{
+			get; set;
+		}
+
+		public double Master_deg
+		{
+			get; set;
+		}
+
+		public double Slave_deg
+		{
+			get; set;
+		}
+	}
+}
