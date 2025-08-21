f7963929fed75c4fc097fb3116af71430bd942a4
王棕賢 <jason.wang@syntecclub.com>
2025-08-21 08:10:38 +0800

    移除不必要的 const 宣告

--- FCLTool/FCLTest.h
+++ FCLTool/FCLTest.h
@@ -3,17 +3,18 @@
 
 #include <fcl/fcl.h>
 
-class FCLTest {
+class FCLTest
+{
 
 public:
 	void FCLTOOL_API AddModel( const std::string &szID,
-		const int triCount, const int *const indexList,
-		const int vertexCount, const double *const vertexList );
+		const int triCount, const int *indexList,
+		const int vertexCount, const double *vertexList );
 
 	bool FCLTOOL_API  CheckCollision( const std::string &szID1, const std::string &szID2,
-		const double *const trsf1, const double *const trsf2 );
+		const double *trsf1, const double *trsf2 );
 
 private:
-	fcl::Transform3f CreateTransform( const double *const trsf );
+	fcl::Transform3f CreateTransform( const double *trsf );
 	std::unordered_map<std::string, std::shared_ptr<fcl::BVHModel<fcl::OBBRSSf>>> m_ModelMap;
 };
