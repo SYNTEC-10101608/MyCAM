9235974bb89f54d12ba429eb63bff7d8d2fe9e43
王棕賢 <jason.wang@syntecclub.com>
2025-08-28 16:56:29 +0800

    重構屬性 get set 與建構 default 參數

--- MyCAM/Data/LeadData.cs
+++ MyCAM/Data/LeadData.cs
@@ -12,17 +12,27 @@
 
 	internal class LeadData
 	{
-		public LeadParam LeadIn { get; set; }
-
-		public LeadParam LeadOut { get; set; }
-
-		public bool IsChangeLeadDirection { get; set; }
-
-		public LeadData( LeadType.LeadLineType leadInType, LeadType.LeadLineType leadOutType, double dLeadInLength, double dLeadInAngle, double dLeadOutLength, double dLeadOutAngle, bool isChangeLeadDirection )
+		public LeadData( LeadType.LeadLineType leadInType = LeadType.LeadLineType.None, LeadType.LeadLineType leadOutType = LeadType.LeadLineType.None,
+			double dLeadInLength = 0, double dLeadInAngle = 0, double dLeadOutLength = 0, double dLeadOutAngle = 0, bool isChangeLeadDirection = false )
 		{
 			LeadIn = new LeadParam( leadInType, dLeadInLength, dLeadInAngle );
 			LeadOut = new LeadParam( leadOutType, dLeadOutLength, dLeadOutAngle );
 			IsChangeLeadDirection = isChangeLeadDirection;
+		}
+
+		public LeadParam LeadIn
+		{
+			get; set;
+		}
+
+		public LeadParam LeadOut
+		{
+			get; set;
+		}
+
+		public bool IsChangeLeadDirection
+		{
+			get; set;
 		}
 
 		public LeadData Clone()
@@ -40,14 +50,26 @@
 
 		internal class LeadParam
 		{
-			public LeadType.LeadLineType Type;
-			public double Length;
-			public double Angle;
 			public LeadParam( LeadType.LeadLineType type = LeadType.LeadLineType.None, double length = 0, double angle = 0 )
 			{
 				Type = type;
 				Length = length;
 				Angle = angle;
+			}
+
+			public LeadType.LeadLineType Type
+			{
+				get; set;
+			}
+
+			public double Length
+			{
+				get; set;
+			}
+
+			public double Angle
+			{
+				get; set;
 			}
 		}
 	}
