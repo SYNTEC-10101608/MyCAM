eb9d5dee71a07a32a3d0b2170b6fedbbcdc42e58
王棕賢 <jason.wang@syntecclub.com>
2025-09-17 10:21:21 +0800

    新增 IEditor 與 EditorBase

---
+++ MyCAM/Editor/IEditor.cs
@@ -0,0 +1,63 @@
+﻿using MyCAM.Data;
+using OCCViewer;
+using System;
+using System.Windows.Forms;
+
+namespace MyCAM.Editor
+{
+	enum EEditorType
+	{
+		CAD,
+		CAM,
+		Post,
+	}
+
+	internal interface IEditor
+	{
+		EEditorType Type
+		{
+			get;
+		}
+
+		void Start();
+
+		void End();
+	}
+
+	internal abstract class EditorBase : IEditor
+	{
+		protected EditorBase( Viewer viewer, TreeView treeView, DataManager cadManager, ViewManager viewManager )
+		{
+			if( viewer == null || treeView == null || cadManager == null || viewManager == null ) {
+				throw new ArgumentNullException( "EditorBase consturcting argument null." );
+			}
+			m_Viewer = viewer;
+			m_TreeView = treeView;
+			m_CADManager = cadManager;
+			m_ViewManager = viewManager;
+		}
+
+		public abstract EEditorType Type
+		{
+			get;
+		}
+
+		public virtual void Start()
+		{
+		}
+
+		public virtual void End()
+		{
+		}
+
+		// editor properties
+		protected readonly Viewer m_Viewer;
+		protected readonly TreeView m_TreeView;
+		protected readonly DataManager m_CADManager;
+		protected readonly ViewManager m_ViewManager;
+
+		// action properties
+		protected IEditorAction m_DefaultAction;
+		protected IEditorAction m_CurrentAction;
+	}
+}
