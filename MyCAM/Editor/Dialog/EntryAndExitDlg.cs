a7962876579cd895a99d643a22d87029fc24d387
王棕賢 <jason.wang@syntecclub.com>
2025-10-29 16:32:56 +0800

    新增 EntryAndExitDlg

---
+++ MyCAM/Editor/Dialog/EntryAndExitDlg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCAM.Editor.Dialog
+{
+	public partial class EntryAndExitDlg : Form
+	{
+		public EntryAndExitDlg()
+		{
+			InitializeComponent();
+		}
+	}
+}
