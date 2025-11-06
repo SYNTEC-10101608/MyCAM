d875c3da02f88daff6193c77e094d6dfd77231f5
林運詠 <andrew.lin@syntecclub.com.tw>
2025-11-06 14:26:28 +0800

    原始CAMData正式被拆成EpathEdge5D+CraftData

---
+++ MyCAM/Data/ContourPathObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace MyCAM.Data
+{
+	internal class ContourPathObject : CraftData
+	{
+		public ContourPathObject( List<PathEdge5D> pathDataList, CraftData craftData )
+		{
+			m_PathEdge5DList = pathDataList;
+			m_CraftData = craftData;
+		}
+
+		public List<PathEdge5D> PathDataList
+		{
+			get
+			{
+				return m_PathEdge5DList;
+			}
+		}
+
+		public CraftData CraftData
+		{
+			get
+			{
+				return m_CraftData;
+			}
+		}
+
+		List<PathEdge5D> m_PathEdge5DList;
+		CraftData m_CraftData = new CraftData();
+	}
+}
