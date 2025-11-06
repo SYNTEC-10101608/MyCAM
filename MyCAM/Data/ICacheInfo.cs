57ccfadde2679ba182f18b7c0d8ae8f5aa82db5e
林運詠 <andrew.lin@syntecclub.com.tw>
2025-11-06 14:52:16 +0800

    把計算結果跟計算方式獨立出來到IcacheInfo以及contourcacheinfo當中

---
+++ MyCAM/Data/ICacheInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace MyCAM.Data
+{
+	internal interface ICacheInfo
+	{
+		List<CAMPoint> LeadInCAMPointList
+		{
+			get;
+		}
+
+		List<CAMPoint> LeadOutCAMPointList
+		{
+			get;
+		}
+
+		List<CAMPoint> OverCutCAMPointList
+		{
+			get;
+		}
+	}
+}
