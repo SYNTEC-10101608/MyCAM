ea85c9f77919f3b2c9fe730f1f551c63b4d5150c
林運詠 <andrew.lin@syntecclub.com.tw>
2025-11-07 20:10:37 +0800

    把封閉取線的isclose拿出來設定

--- MyCAM/Data/ContourPathObject.cs
+++ MyCAM/Data/ContourPathObject.cs
@@ -1,16 +1,27 @@
-﻿using OCC.TopoDS;
+﻿using OCC.BRep;
+using OCC.gp;
+using OCC.TopExp;
+using OCC.TopoDS;
 using System.Collections.Generic;
 
 namespace MyCAM.Data
 {
 	internal class ContourPathObject : PathObject
 	{
-		public ContourPathObject( string szUID, TopoDS_Shape shapeData, List<PathEdge5D> pathDataList, CraftData craftData )
+		public ContourPathObject( DataManager dataManager, string szUID, TopoDS_Shape shapeData, List<PathEdge5D> pathDataList )
 			: base( szUID, shapeData, PathType.Contour )
 		{
 			m_PathEdge5DList = pathDataList;
-			m_CraftData = craftData;
-			m_ContourCacheInfo = new ContourCacheInfo( szUID, m_PathEdge5DList, craftData );
+
+			TopoDS_Vertex startVertex = new TopoDS_Vertex();
+			TopoDS_Vertex endVertex = new TopoDS_Vertex();
+			TopExp.Vertices( TopoDS.ToWire( shapeData ), ref startVertex, ref endVertex );
+			gp_Pnt startPoint = BRep_Tool.Pnt( TopoDS.ToVertex( startVertex ) );
+			gp_Pnt endPoint = BRep_Tool.Pnt( TopoDS.ToVertex( endVertex ) );
+			bool isClosed = startPoint.IsEqual( endPoint, 1e-3 );
+
+			m_CraftData = new CraftData( szUID, isClosed );
+			m_ContourCacheInfo = new ContourCacheInfo( dataManager, szUID, m_PathEdge5DList, m_CraftData );
 		}
 
 		public List<PathEdge5D> PathDataList
@@ -36,6 +47,7 @@ namespace MyCAM.Data
 				return m_ContourCacheInfo;
 			}
 		}
+
 
 		List<PathEdge5D> m_PathEdge5DList;
 		CraftData m_CraftData;
