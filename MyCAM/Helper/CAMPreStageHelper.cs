e31c68a306800792db66894495beacd8b0577cd0
JasonWang0412 <jason970411@gmail.com>
2025-11-29 20:06:43 +0800

    rename git bug 處理 2

--- MyCAM/Helper/CAMPreStageHelper.cs
+++ MyCAM/Helper/CAMPreStageHelper.cs
@@ -66,7 +66,7 @@ namespace MyCAM.Helper
 		}
 	}
 
-	public class CAMPretageHelper
+	public class CAMPreStageHelper
 	{
 		public static List<CAMPointInfo> FlattenCADSegmentsToCAMPointInfo(
 			List<ICADSegment> cadSegmentList, CraftData craftData, bool isClosed )
