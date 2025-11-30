fe94354422c02c0a3ca1ce5e86d4a0b7136af786
JasonWang0412 <jason970411@gmail.com>
2025-11-30 18:40:39 +0800

    test

--- MyCAM/Helper/CAMPostStageHelper.cs
+++ MyCAM/Helper/CAMPostStageHelper.cs
@@ -1,5 +1,6 @@
 ﻿using MyCAM.Data;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MyCAM.Helper
 {
@@ -47,24 +48,63 @@ namespace MyCAM.Helper
 			for( int i = 0; i < pathCAMInfo.Count; i++ ) {
 
 				// 檢查 i 的 sharing point 是否為 null，若是，流程有問題，回傳 false
+				if( pathCAMInfo[ i ].SharingPoint == null ) {
+					return false;
+				}
+				List<ICAMSegInfo> tempList = new List<ICAMSegInfo>();
 
 				// 往後找到 sharing point 不為 null 的 index j，若到終點還沒找到，表示是最後一個段落，取 j 到最後一個點，然後取 [0]
+				int j = i + 1;
+				while( j < pathCAMInfo.Count && pathCAMInfo[ j ].SharingPoint == null ) {
+					tempList.Add( pathCAMInfo[ j ] );
+					j++;
+				}
+				if( j == pathCAMInfo.Count ) {
+					tempList.Add( pathCAMInfo[ 0 ] );
+				}
+				else {
+					tempList.Add( pathCAMInfo[ j ] );
+				}
 
 				// 檢查 i+1 到 j 的 MainPoint 和 MainPointSeg 是否為 null，若是，流程有問題，回傳 false
-
 				// 檢查 i+1 到 j 的 MainPointSeg 是否為同一個，且與 i 的 SharingPointSeg 相同，若不是，流程有問題，回傳 false
+				foreach( var camSegInfo in tempList ) {
+					if( camSegInfo.MainPoint == null || camSegInfo.MainPointSeg == null
+						|| camSegInfo.MainPointSeg != pathCAMInfo[ i ].SharingPointSeg ) {
+						return false;
+					}
+				}
 
-				// 建立新的 CAMSegment，起點為 i 的 SharingPoint，終點為 j 的 MainPoint，中間點為 i+1 到 j-1 的 MainPoint
-
-				// 將新的 CAMSegment 加入 PathCAMSegList
+				// 建立新的 CAMSegment，起點為 i 的 SharingPoint，後續為 i+1 到 j 的 MainPoint
+				List<CAMPoint2> camPointList = new List<CAMPoint2> { pathCAMInfo[ i ].SharingPoint };
+				foreach( var camSegInfo in tempList ) {
+					camPointList.Add( camSegInfo.MainPoint );
+				}
+				double segmentLength = pathCAMInfo[ i ].SharingPointSeg.SegmentLength;
+				double subSegmentLength = pathCAMInfo[ i ].SharingPointSeg.SubSegmentLength;
+				double subChordLength = pathCAMInfo[ i ].SharingPointSeg.SubChordLength;
+				if( pathCAMInfo[ i ].SharingPointSeg.SegmentType == ESegmentType.Line ) {
+					ICAMSegment newSegment = new LineCAMSegment( camPointList, segmentLength, subSegmentLength, subChordLength );
+					PathCAMSegList.Add( newSegment );
+				}
+				else {
+					ICAMSegment newSegment = new ArcCAMSegment( camPointList, segmentLength, subSegmentLength, subChordLength );
+					PathCAMSegList.Add( newSegment );
+				}
 
 				// 若 j 的 IsToolVecPnt 為 true，將此 CAMSegment 的 index 加入 CtrlSegIdx
+				if( tempList.Last().IsToolVecPnt ) {
+					CtrlSegIdx.Add( PathCAMSegList.Count - 1 );
+				}
 
 				// 若 j 的 IsStartPnt 為 true，標記此 CAMSegment 的 index，稍後使用
+				//if( pathCAMInfo[ j ].IsStartPnt ) {
+				//	// 標記邏輯可根據需求實現
+				//}
 
 				// 迴圈從 j 繼續，做一樣的事情，直到處理完整個 pathCAMInfo
+				i = j - 1; // 調整 i 的位置，避免重複處理
 			}
-
 			return true;
 		}
 	}
