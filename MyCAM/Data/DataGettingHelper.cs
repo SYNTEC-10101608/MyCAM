440352deb8ef832d413df75b6e60a990c08ff522
林運詠 <andrew.lin@syntecclub.com.tw>
2025-12-07 20:54:39 +0800

    把透過ID取得GeomData的方式隔離到DataGettingHelper

---
+++ MyCAM/Data/DataGettingHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace MyCAM.Data
+{
+	public static class DataGettingHelper
+	{
+		static DataManager m_DataManager;
+
+		internal static void Initialize( DataManager dataManager )
+		{
+			m_DataManager = dataManager;
+		}
+
+		public static IGeomData GetGeomDataByID( string pathID )
+		{
+			Dictionary<string, PathObject> pathObjectList = m_DataManager.GetPathObjectDictionary();
+			if( pathObjectList.TryGetValue( pathID, out PathObject pathObject ) ) {
+				switch( pathObject.PathType ) {
+					case PathType.Circle:
+						return ( pathObject as CirclePathObject ).CircleGeomData;
+					case PathType.Rectangle:
+						return ( pathObject as RectanglePathObject ).RectangleGeomData;
+					case PathType.Contour:
+					default:
+						return ( pathObject as ContourPathObject ).ContourGeomData;
+				}
+			}
+			return null;
+		}
+	}
+}
