e305447a140d32e099a9b8b9e242d3f3ee314f9d
林運詠 <andrew.lin@syntecclub.com.tw>
2025-12-16 15:40:26 +0800

    加入第一版引線(直線/圓弧二選一)

--- MyCAM/PathCache/Computation/RectangleCache.cs
+++ MyCAM/PathCache/Computation/RectangleCache.cs
@@ -43,6 +43,11 @@ namespace MyCAM.PathCache
 			List<IOrientationPoint> camPointOverCutList = m_StartCAMPointList.Cast<IOrientationPoint>().ToList();
 			OverCutHelper.SetStdPatternOverCut( m_RefCoord, m_RectangleGeomData, camPointOverCutList, m_CraftData.OverCutLength, out List<IOrientationPoint> overCutPointList );
 			m_OverCutCAMPointList = overCutPointList.Cast<CAMPoint>().ToList();
+
+			// set lead
+			List<IOrientationPoint> mainPointList = m_StartCAMPointList.Cast<IOrientationPoint>().ToList();
+			LeadHelper.SetLeadIn( mainPointList, out List<IOrientationPoint> leadInPointList, m_CraftData.LeadData, m_CraftData.IsPathReverse );
+			m_LeadInCAMPointList = leadInPointList.Cast<CAMPoint>().ToList();
 		}
 
 		void SetStartPoint()
