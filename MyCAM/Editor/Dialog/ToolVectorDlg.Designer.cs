1df36edfa67b220672b62f59459c18ed1077ca0f
董塘筠 <hoya.tung@syntecclub.com.tw>
2025-12-16 12:48:00 +0800

    Merge branch 'AUTO-17077_f_ToolVecExpanded' into '0.6.0'
    by 塘筠
    
    新增 保持前姿態編修點功能
    新增 姿態朝Z功能
    調整 傾角設定值考慮路徑方向
    調整 傾角設置數值區間為±60度

--- MyCAM/Editor/Dialog/ToolVectorDlg.Designer.cs
+++ MyCAM/Editor/Dialog/ToolVectorDlg.Designer.cs
@@ -28,63 +28,60 @@
 		private void InitializeComponent()
 		{
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
+			this.m_lblBDeg = new System.Windows.Forms.Label();
 			this.m_tbxAngleA = new System.Windows.Forms.TextBox();
 			this.m_tbxAngleB = new System.Windows.Forms.TextBox();
 			this.m_btnOK = new System.Windows.Forms.Button();
 			this.m_btnRemove = new System.Windows.Forms.Button();
+			this.m_btnKeep = new System.Windows.Forms.Button();
+			this.m_btnZDir = new System.Windows.Forms.Button();
+			this.m_lblADeg = new System.Windows.Forms.Label();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel1
 			// 
 			this.tableLayoutPanel1.ColumnCount = 3;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 52F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 52F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 52F));
-			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 108F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+			this.tableLayoutPanel1.Controls.Add(this.m_lblBDeg, 0, 1);
 			this.tableLayoutPanel1.Controls.Add(this.m_tbxAngleA, 1, 0);
 			this.tableLayoutPanel1.Controls.Add(this.m_tbxAngleB, 1, 1);
-			this.tableLayoutPanel1.Controls.Add(this.m_btnOK, 1, 3);
-			this.tableLayoutPanel1.Controls.Add(this.m_btnRemove, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.m_btnOK, 2, 5);
+			this.tableLayoutPanel1.Controls.Add(this.m_btnRemove, 2, 4);
+			this.tableLayoutPanel1.Controls.Add(this.m_btnKeep, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.m_btnZDir, 1, 3);
+			this.tableLayoutPanel1.Controls.Add(this.m_lblADeg, 0, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 5;
+			this.tableLayoutPanel1.RowCount = 6;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(122, 111);
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(226, 178);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
-			// label1
+			// m_lblBDeg
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(2, 0);
-			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(41, 12);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "前傾角";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(2, 24);
-			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(41, 12);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "側傾角";
+			this.m_lblBDeg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.m_lblBDeg.AutoSize = true;
+			this.m_lblBDeg.Location = new System.Drawing.Point(2, 24);
+			this.m_lblBDeg.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.m_lblBDeg.Name = "m_lblBDeg";
+			this.m_lblBDeg.Size = new System.Drawing.Size(41, 24);
+			this.m_lblBDeg.TabIndex = 1;
+			this.m_lblBDeg.Text = "側傾角";
 			// 
 			// m_tbxAngleA
 			// 
-			this.m_tbxAngleA.Location = new System.Drawing.Point(54, 2);
+			this.m_tbxAngleA.Location = new System.Drawing.Point(56, 2);
 			this.m_tbxAngleA.Margin = new System.Windows.Forms.Padding(2);
 			this.m_tbxAngleA.Name = "m_tbxAngleA";
 			this.m_tbxAngleA.Size = new System.Drawing.Size(48, 22);
@@ -94,7 +91,7 @@
 			// 
 			// m_tbxAngleB
 			// 
-			this.m_tbxAngleB.Location = new System.Drawing.Point(54, 26);
+			this.m_tbxAngleB.Location = new System.Drawing.Point(56, 26);
 			this.m_tbxAngleB.Margin = new System.Windows.Forms.Padding(2);
 			this.m_tbxAngleB.Name = "m_tbxAngleB";
 			this.m_tbxAngleB.Size = new System.Drawing.Size(48, 22);
@@ -104,30 +101,61 @@
 			// 
 			// m_btnOK
 			// 
-			this.m_btnOK.Location = new System.Drawing.Point(54, 74);
-			this.m_btnOK.Margin = new System.Windows.Forms.Padding(2);
+			this.m_btnOK.Location = new System.Drawing.Point(165, 150);
 			this.m_btnOK.Name = "m_btnOK";
-			this.m_btnOK.Size = new System.Drawing.Size(48, 18);
-			this.m_btnOK.TabIndex = 4;
+			this.m_btnOK.Size = new System.Drawing.Size(58, 25);
+			this.m_btnOK.TabIndex = 8;
 			this.m_btnOK.Text = "OK";
 			this.m_btnOK.UseVisualStyleBackColor = true;
 			this.m_btnOK.Click += new System.EventHandler(this.m_btnOK_Click);
 			// 
 			// m_btnRemove
 			// 
-			this.m_btnRemove.Location = new System.Drawing.Point(55, 51);
+			this.m_btnRemove.Location = new System.Drawing.Point(165, 114);
 			this.m_btnRemove.Name = "m_btnRemove";
-			this.m_btnRemove.Size = new System.Drawing.Size(46, 18);
-			this.m_btnRemove.TabIndex = 5;
+			this.m_btnRemove.Size = new System.Drawing.Size(58, 30);
+			this.m_btnRemove.TabIndex = 9;
 			this.m_btnRemove.Text = "刪除";
 			this.m_btnRemove.UseVisualStyleBackColor = true;
 			this.m_btnRemove.Click += new System.EventHandler(this.m_btnRemove_Click);
+			// 
+			// m_btnKeep
+			// 
+			this.m_btnKeep.Location = new System.Drawing.Point(57, 51);
+			this.m_btnKeep.Name = "m_btnKeep";
+			this.m_btnKeep.Size = new System.Drawing.Size(102, 27);
+			this.m_btnKeep.TabIndex = 10;
+			this.m_btnKeep.Text = "保持前編修姿態";
+			this.m_btnKeep.UseVisualStyleBackColor = true;
+			this.m_btnKeep.Click += new System.EventHandler(this.m_btnKeep_Click);
+			// 
+			// m_btnZDir
+			// 
+			this.m_btnZDir.Location = new System.Drawing.Point(57, 84);
+			this.m_btnZDir.Name = "m_btnZDir";
+			this.m_btnZDir.Size = new System.Drawing.Size(102, 23);
+			this.m_btnZDir.TabIndex = 11;
+			this.m_btnZDir.Text = "朝Z方向";
+			this.m_btnZDir.UseVisualStyleBackColor = true;
+			this.m_btnZDir.Click += new System.EventHandler(this.m_btnZDir_Click);
+			// 
+			// m_lblADeg
+			// 
+			this.m_lblADeg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.m_lblADeg.AutoSize = true;
+			this.m_lblADeg.Location = new System.Drawing.Point(2, 0);
+			this.m_lblADeg.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.m_lblADeg.Name = "m_lblADeg";
+			this.m_lblADeg.Size = new System.Drawing.Size(41, 24);
+			this.m_lblADeg.TabIndex = 0;
+			this.m_lblADeg.Text = "前傾角";
 			// 
 			// ToolVectorDlg
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(122, 111);
+			this.ClientSize = new System.Drawing.Size(226, 178);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Margin = new System.Windows.Forms.Padding(2);
@@ -146,11 +174,13 @@
 		#endregion
 
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label m_lblADeg;
+		private System.Windows.Forms.Label m_lblBDeg;
 		private System.Windows.Forms.TextBox m_tbxAngleA;
 		private System.Windows.Forms.TextBox m_tbxAngleB;
 		private System.Windows.Forms.Button m_btnOK;
 		private System.Windows.Forms.Button m_btnRemove;
+		private System.Windows.Forms.Button m_btnKeep;
+		private System.Windows.Forms.Button m_btnZDir;
 	}
 }
