f5b0c945bcbe5c3225c6fc52dfbf7ba428c5ad49
董塘筠 <hoya.tung@syntecclub.com.tw>
2026-03-05 18:49:59 +0800

    Merge branch 'AUTO-20239_f_PCNCLayoutImprove' into '0.13.0'
    by 塘筠
    
    調整 控制器畫面邊框顯示
    新增 橫式螢幕控制器畫面視窗
    新增 默認連線IP自動設置

---
+++ MyCAM/Editor/Dialog/ControllerDlg.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace MyCAM.Editor.Dialog
+{
+	partial class ControllerDlg
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing && ( components != null ) ) {
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// ControllerDlg
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1178, 843);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "ControllerDlg";
+			this.ShowIcon = false;
+			this.Text = "控制器";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
