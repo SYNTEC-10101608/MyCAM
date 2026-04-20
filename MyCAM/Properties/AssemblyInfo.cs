9ce0f1dd8436e489ed14a4859a6b604809168e47
王棕賢 <jason.wang@syntecclub.com>
2026-04-20 10:59:22 +0800

    RLS_0.17.1

--- MyCAM/Properties/AssemblyInfo.cs
+++ MyCAM/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "0.17.0.0" )]
-[assembly: AssemblyFileVersion( "0.17.0.0" )]
+[assembly: AssemblyVersion( "0.17.1.0" )]
+[assembly: AssemblyFileVersion( "0.17.1.0" )]
