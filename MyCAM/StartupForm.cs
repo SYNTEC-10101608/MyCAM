ed4ac8f7fe352e457b992d4cd92cdfbdaa86b7d3
王棕賢 <jason.wang@syntecclub.com>
2026-04-23 13:21:32 +0800

    把入口點還給姿態反向

--- MyCAM/StartupForm.cs
+++ MyCAM/StartupForm.cs
@@ -364,8 +364,7 @@ namespace MyCAM
 
 		void m_tsbTooVecReverse_Click( object sender, EventArgs e )
 		{
-			//m_CAMEditor.SetToolVecReverse();
-			m_CAMEditor.SetContourEdit();
+			m_CAMEditor.SetToolVecReverse();
 		}
 
 		void m_tsbFixedToolVec_Click( object sender, EventArgs e )
