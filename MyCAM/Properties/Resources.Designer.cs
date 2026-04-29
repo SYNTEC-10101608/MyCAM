cd2569dbf402f03d50714b80777a921f8207a8fe
王棕賢 <jason.wang@syntecclub.com>
2026-04-29 15:31:43 +0800

    將 bndbox 以 res 形式導入

--- MyCAM/Properties/Resources.Designer.cs
+++ MyCAM/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace MyCAM.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CAD_BoundingBoxCenter {
+            get {
+                object obj = ResourceManager.GetObject("CAD_BoundingBoxCenter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap CAD_ManualTransform {
             get {
                 object obj = ResourceManager.GetObject("CAD_ManualTransform", resourceCulture);
